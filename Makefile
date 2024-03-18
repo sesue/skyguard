447b0879122762a1f531bc7ae84c87efc3cea7d9
Samuel Escue <sescue@usc.edu>
2024-03-18 15:38:38 -0700

    Makefile

---
+++ Makefile
@@ -0,0 +1,5 @@
+clean:
+	rm data/*.txt
+	rm data/video/*.h264
+	rm data/gps/*.txt
+	rm data/metadata/*.json
--- test_code/led_test.py
+++ test_code/led_test.py
@@ -5,14 +5,11 @@ import time
 
 #--Input/Output Constants--
 CAMERA_LED = 17
-BUTTON_PIN = 16
 #--------------------------
 
 #Initialize Input/Output
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(CAMERA_LED,GPIO.OUT)
-GPIO.setup(BUTTON_PIN,GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
 
 flag = True
 while(True):
